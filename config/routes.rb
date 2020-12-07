8b8b8b88255245688abad698989fc7cd5d2fd307
UJr-creator <rokumugi123good@gmail.com>
2020-12-07 14:18:54 +0000

    feat:Userコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/new'
   root to: 'toppages#index'
+  get "signup", to: "users#new"
+  resources :users, only: %i[show, create, edit, update]
+
 end
