26441139fcd9ee8b0e27391facb644d806fd91af
UJr-creator <rokumugi123good@gmail.com>
2020-12-07 14:57:19 +0000

    feat:新規登録機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
-
   end
 
   def create
@@ -28,6 +27,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/new'
   root to: 'toppages#index'
   get "signup", to: "users#new"
-  resources :users, only: %i[show, create, edit, update]
+  resources :users, only: %i[show create edit update]
 end
