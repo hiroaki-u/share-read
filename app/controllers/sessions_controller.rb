b48725d275bc9ef210030ac912d68eaf1bb61562
UJr-creator <rokumugi123good@gmail.com>
2020-12-10 11:23:06 +0000

    ログイン機能実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,29 @@ class SessionsController < ApplicationController
   end
 
   def create
+    email = params[:session][:email]
+    password = params[:session][:password]
+    if login(email, password)
+      flash[:success] = "ログインしました"
+      redirect_to root_url
+    else
+      flash.now[:danger] = "ログインできませんでした"
+      render :new
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def login(email, password)
+    @user = User.find_by(email: email)
+    if @user && @user.authenticate(password)
+      session[:id] = @user.id
+      return true
+    else
+      return false
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+
   root to: 'toppages#index'
   get "signup", to: "users#new"
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#new"
+  get "logout", to: "sessions#destroy"
   resources :users, only: %i[show create edit update]
 end
