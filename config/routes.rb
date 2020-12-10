94d9b6bf88ba1410ddfc9a06340630bd3f1d7da8
UJr-creator <rokumugi123good@gmail.com>
2020-12-10 14:45:11 +0000

    feat:ヘッダーにログイン、ログアウト、新規登録ボタン追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
   root to: 'toppages#index'
-  get "signup", to: "users#new"
-  get "login", to: "sessions#new"
-  post "login", to: "sessions#new"
-  get "logout", to: "sessions#destroy"
   resources :users, only: %i[show create edit update]
+  get "signup", to: "users#new"
+
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  post "logout", to: "sessions#destroy"
+
 end
