4e473c00d8cefbcb6909d287f93933ef118ee827
UJr-creator <rokumugi123good@gmail.com>
2020-12-18 15:33:15 +0900

    api取得,Bookテーブルへのデータ格納

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,49 @@
 class BooksController < ApplicationController
+
+  def show
+    @book = Book.find_by(isbn: params[:id])
+  end
+  
   def search
-    if params[:keyword]
-      @books = RakutenWebService::Books::Book.search(title: params[:keyword])
-    else
-      flash[:notice] = "検索結果はありませんでした"
+    @books = []
+    @title = params[:title]
+    if @title.present?
+      results = RakutenWebService::Books::Book.search({
+        title: @title,
+      })
+      results.each do |result|
+        book = Book.new(read(result))
+        @books << book
+      end
+    end
+    @books.each do |book|
+      book.save
     end
   end
 
-  def show
+  def create
+    @book = Book.find_or_initialize_by(isbn: params[:isbn])
+
+    unless @book.present?
+      results = RakutenWebService::Books::Book.search(isbn: @book.isbn)
+      @book = Book.new(read(results.first))
+      @book.save
+    end
+  end
+
+  private
+  def read(result)
+    title = result["title"]
+    author = result["author"]
+    url = result["itemUrl"]
+    isbn = result["isbn"]
+    image_url = result["mediumImageUrl"].gsub('?_ex=120x120', '')
+    
+    {
+      title: title,
+      url: url,
+      isbn: isbn,
+      image_url: image_url
+    }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_13_135207) do
+ActiveRecord::Schema.define(version: 2020_12_18_052640) do
+
+  create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.bigint "isbn"
+    t.string "url"
+    t.string "image_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.text "content"
