f78dedf240fc730223420cd858e584334d156301
UJr-creator <rokumugi123good@gmail.com>
2020-12-22 13:37:31 +0000

    feat:お気に入り機能実装

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,18 @@
+class FavoritesController < ApplicationController
+  before_action :require_login
+  before_action :set_review
+
+  def create
+    current_user.favor(@review)
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    current_user.unfavor(@review)
+    redirect_back(fallback_location: root_path)
+  end
+
+  def set_review
+    @review = Review.find(params[:review_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,25 @@
 Rails.application.routes.draw do
 
+  get 'favorites/create'
+  get 'favorites/destroy'
   root to: 'toppages#index'
-  resources :users, only: %i[show create edit update] do
-    member do
-      get :followings
-      get :followers
-    end
-  end
-  resources :relationships, only: [:create, :destroy]
-  get "signup", to: "users#new"
 
+  get "signup", to: "users#new"
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
   post 'guest_login', to: "guest_sessions#create"
-  
+
+  resources :users, only: %i[show create edit update] do
+    member do
+      get :followings
+      get :followers
+      get :favorites
+    end
+  end
+
+  resources :relationships, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy]
 
   get 'books/search', to: "books#search"
   resources :books, only: %i[show] do 
